2cb041f9ed91bd247f1a9bd59b79081c3f5fdffc
Florian Witteler <github@flwi.de>
2024-12-05 12:11:16 +0100

    solved day-05 part 1

--- adventofcode-2024/day-05/src/part1.rs
+++ adventofcode-2024/day-05/src/part1.rs
@@ -1,6 +1,7 @@
+use itertools::Itertools;
 use miette::miette;
-use nom::character::complete::{char, newline};
 use nom::character::complete;
+use nom::character::complete::{char, newline};
 use nom::multi::{many_m_n, separated_list1};
 use nom::sequence::separated_pair;
 use nom::IResult;
@@ -8,12 +9,57 @@ use nom::Parser;
 
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String> {
-    let (_rest, (ordering_rules, pages)) = parse(input)
-        .map_err(|e| miette!("parse failed {}", e))?;
+    let (_rest, (ordering_rules, pages_list)) =
+        parse(input).map_err(|e| miette!("parse failed {}", e))?;
 
-    dbg!(ordering_rules);
-    dbg!(pages);
-    Ok("???".to_string())
+    dbg!(&ordering_rules);
+    dbg!(&pages_list);
+
+    let valid_updates = pages_list
+        .iter()
+        .filter(|pages| has_correct_order(*pages, ordering_rules.as_slice()))
+        .collect_vec();
+    dbg!(&valid_updates);
+
+    let result: i32 = valid_updates
+        .iter()
+        .map(|PageNumbersForUpdate(pages)| middle_number(pages).unwrap())
+        .sum();
+
+    Ok(result.to_string())
+}
+
+fn middle_number(list: &Vec<i32>) -> Option<i32> {
+    let idx = list.len() / 2;
+    list.get(idx).cloned()
+}
+
+fn has_correct_order(pages: &PageNumbersForUpdate, rules: &[PageOrderingRule]) -> bool {
+    /*
+    (75,47,61,53,29)
+    75 is correctly first because there are rules that put each other page after it: 75|47, 75|61, 75|53, and 75|29.
+     */
+    for PageOrderingRule(first, second) in rules {
+        let maybe_first_idx = pages
+            .0
+            .iter()
+            .find_position(|p| *p == first)
+            .map(|(idx, _)| idx);
+        let maybe_second_idx = pages
+            .0
+            .iter()
+            .find_position(|p| *p == second)
+            .map(|(idx, _)| idx);
+        match maybe_first_idx.zip(maybe_second_idx) {
+            None => {}
+            Some((idx_1, idx_2)) => {
+                if idx_1 > idx_2 {
+                    return false;
+                }
+            }
+        }
+    }
+    true
 }
 
 #[derive(Debug)]
@@ -23,14 +69,21 @@ struct PageOrderingRule(i32, i32);
 struct PageNumbersForUpdate(Vec<i32>);
 
 fn parse(input: &str) -> IResult<&str, (Vec<PageOrderingRule>, Vec<PageNumbersForUpdate>)> {
-    let ordering_rules_parser = separated_list1(newline, separated_pair(complete::i32, char('|'), complete::i32).map(|(n1, n2)| PageOrderingRule(n1, n2)));
-    let pages_parser = separated_list1(newline, separated_list1(char(','), complete::i32).map(|pages| PageNumbersForUpdate(pages)));
+    let ordering_rules_parser = separated_list1(
+        newline,
+        separated_pair(complete::i32, char('|'), complete::i32)
+            .map(|(n1, n2)| PageOrderingRule(n1, n2)),
+    );
+    let pages_parser = separated_list1(
+        newline,
+        separated_list1(char(','), complete::i32).map(|pages| PageNumbersForUpdate(pages)),
+    );
 
-    let (remaining, result) = separated_pair(ordering_rules_parser, many_m_n(2, 2, newline), pages_parser)(input)?;
+    let (remaining, result) =
+        separated_pair(ordering_rules_parser, many_m_n(2, 2, newline), pages_parser)(input)?;
 
     Ok((remaining, result))
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -68,8 +121,8 @@ mod tests {
 61,13,29
 97,13,75,29,47
         "#
-            .trim();
-        assert_eq!("", process(input)?);
+        .trim();
+        assert_eq!("143", process(input)?);
         Ok(())
     }
 }
