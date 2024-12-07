59ce29a61fb3afb1b6e227a50842ef640cc32cfd
Florian Witteler <github@flwi.de>
2024-12-07 13:15:15 +0100

    refactoring to prepare for part 2

--- adventofcode-2024/day-06/src/lib.rs
+++ adventofcode-2024/day-06/src/lib.rs
@@ -1,2 +1,85 @@
+use glam::IVec2;
+use std::collections::{HashMap, HashSet};
+use std::ops::Add;
+
 pub mod part1;
 pub mod part2;
+
+fn parse_map(input: &str) -> (Vec<Vec<bool>>, IVec2, IVec2) {
+    let mut occupancy_map = Vec::new();
+    let mut location: Option<IVec2> = None;
+    let mut direction: Option<IVec2> = None;
+
+    let directions = HashMap::from([
+        ('>', IVec2::new(1, 0)),
+        ('v', IVec2::new(0, 1)),
+        ('<', IVec2::new(-1, 0)),
+        ('^', IVec2::new(0, -1)),
+    ]);
+
+    for (y, line_str) in input.lines().enumerate() {
+        if &occupancy_map.len() <= &y {
+            &occupancy_map.push(Vec::new());
+        }
+        let mut line = &mut occupancy_map[y];
+        for (x, map_char) in line_str.char_indices() {
+            if map_char == '#' {
+                line.push(true)
+            } else {
+                if let Some(dir) = directions.get(&map_char) {
+                    location = Some(IVec2::new(x as i32, y as i32));
+                    direction = Some(*dir);
+                }
+                line.push(false)
+            }
+        }
+    }
+    (occupancy_map, location.unwrap(), direction.unwrap())
+}
+
+fn walk_off_the_earth<F>(
+    occupancy_map: &Vec<Vec<bool>>,
+    location: &IVec2,
+    direction: &IVec2,
+    in_bounds: F,
+) -> HashSet<IVec2>
+where
+    F: Fn(IVec2) -> bool,
+{
+    let mut location = *location;
+    let mut direction = *direction;
+    let mut visited: HashSet<IVec2> = HashSet::from([location]);
+    loop {
+        //FIXME: might need to rotate multiple times if you hit a dead-end
+        let (new_location, new_direction) = perform_step(occupancy_map, &location, &direction);
+        location = new_location;
+        direction = new_direction;
+        if !in_bounds(location) {
+            return visited;
+        }
+        visited.insert(location);
+    }
+}
+
+fn perform_step(occupancy_map: &Vec<Vec<bool>>, location: &IVec2, direction: &IVec2) -> (IVec2, IVec2) {
+    let lookup_location = location.add(direction);
+    let is_occupied = occupancy_map
+        .get(lookup_location.y as usize)
+        .and_then(|row| row.get(lookup_location.x as usize))
+        .unwrap_or(&false);
+    let new_direction = if *is_occupied {
+        IVec2::new(-direction.y, direction.x) // rotate 90° CW if you hit an obstacle. Glam's positive y-axis points up, so we can't use their internal rotation stuff
+    } else {
+        *direction
+    };
+
+    let new_location = if *is_occupied {
+        *location
+    } else {
+        location.add(direction)
+    };
+
+    (new_location, new_direction)
+}
+
+
--- adventofcode-2024/day-06/src/part1.rs
+++ adventofcode-2024/day-06/src/part1.rs
@@ -1,85 +1,24 @@
+use crate::{parse_map, walk_off_the_earth};
 use glam::IVec2;
 use itertools::Itertools;
-use std::collections::{HashMap, HashSet};
 use std::ops::{Add, Neg};
 
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String> {
     let (occupancy_map, location, direction) = parse_map(input);
 
-    dbg!(&occupancy_map);
-    dbg!(&location);
-    dbg!(&direction);
+    let height = occupancy_map.len();
+    let width = occupancy_map[0].len();
 
-    let visited = walk_off_the_earth(&occupancy_map, &location, &direction);
+    let map_dimensions = IVec2::new(width as i32, height as i32);
+
+    let is_in_bounds = |loc: IVec2| loc.x >= 0 && loc.y >= 0 && loc.x < map_dimensions.x && loc.y < map_dimensions.y;
+
+    let visited = walk_off_the_earth(&occupancy_map, &location, &direction, is_in_bounds);
 
     let result = visited.len();
 
     Ok(result.to_string())
-}
-
-fn walk_off_the_earth(
-    occupancy_map: &Vec<Vec<bool>>,
-    location: &IVec2,
-    direction: &IVec2,
-) -> HashSet<IVec2> {
-    let height = occupancy_map.len() as i32;
-    let width = occupancy_map[0].len() as i32;
-
-    let mut location = *location;
-    let mut direction = *direction;
-    let mut visited: HashSet<IVec2> = HashSet::from([location]);
-    loop {
-        //FIXME: might need to rotate multiple times if you hit a dead-end
-        println!("Exploring {:?} in direction {:?}", location, direction);
-        let lookup_location = location.add(direction);
-        let is_occupied = occupancy_map
-            .get(lookup_location.y as usize)
-            .and_then(|row| row.get(lookup_location.x as usize))
-            .unwrap_or(&false);
-        if *is_occupied {
-            direction = IVec2::new(-direction.y, direction.x); // rotate 90° CW if you hit an obstacle. Glam's positive y-axis points up, so we can't use their internal rotation stuff
-            println!("Hit obstacle - new direction is {:?}", direction);
-        }
-        location = location.add(direction);
-        if location.y < 0 || location.y >= height || location.x < 0 || location.x >= width {
-            println!("Fell off the earth at {:}", location);
-            return visited;
-        }
-        visited.insert(location);
-    }
-}
-
-fn parse_map(input: &str) -> (Vec<Vec<bool>>, IVec2, IVec2) {
-    let mut occupancy_map = Vec::new();
-    let mut location: Option<IVec2> = None;
-    let mut direction: Option<IVec2> = None;
-
-    let directions = HashMap::from([
-        ('>', IVec2::new(1, 0)),
-        ('v', IVec2::new(0, 1)),
-        ('<', IVec2::new(-1, 0)),
-        ('^', IVec2::new(0, -1)),
-    ]);
-
-    for (y, line_str) in input.lines().enumerate() {
-        if &occupancy_map.len() <= &y {
-            &occupancy_map.push(Vec::new());
-        }
-        let mut line = &mut occupancy_map[y];
-        for (x, map_char) in line_str.char_indices() {
-            if map_char == '#' {
-                line.push(true)
-            } else {
-                if let Some(dir) = directions.get(&map_char) {
-                    location = Some(IVec2::new(x as i32, y as i32));
-                    direction = Some(*dir);
-                }
-                line.push(false)
-            }
-        }
-    }
-    (occupancy_map, location.unwrap(), direction.unwrap())
 }
 
 #[cfg(test)]
