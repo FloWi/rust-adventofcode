e72655949d4838779b731a6e9d9746962cc90783
Florian Witteler <github@flwi.de>
2024-12-09 12:33:02 +0100

    solved day-09 part 1

--- adventofcode-2024/day-09/src/part1.rs
+++ adventofcode-2024/day-09/src/part1.rs
@@ -1,17 +1,62 @@
 use itertools::{repeat_n, Itertools};
 
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    //
-    dbg!(i16::MAX);
-    todo!("day 01 - part 1");
+pub fn process(input: &str) -> miette::Result<String> {
+    let mut disk_blocks = parse_disk_blocks(input.trim());
+    let compaction_result = compact_until_finished(&mut disk_blocks);
+    assert!(compaction_result);
+
+    let result = compute_checksum(&disk_blocks);
+
+    Ok(result.to_string())
 }
 
-fn compact_files(disk_blocks: &mut Vec<Option<u16>>) {
+#[derive(PartialOrd, PartialEq)]
+enum CompactionResult {
+    Finished,
+    OneStepDone,
+    Error,
+}
+
+fn compact_one_block(disk_blocks: &mut Vec<Option<u16>>) -> CompactionResult {
     if let Some(first_empty_idx) = disk_blocks.iter().position(|b| b.is_none()) {
         if let Some(last_nonempty_idx) = disk_blocks.iter().rposition(|b| b.is_some()) {
-            dbg!(first_empty_idx, last_nonempty_idx);
+            if first_empty_idx > last_nonempty_idx {
+                return CompactionResult::Finished;
+            }
             disk_blocks.swap(first_empty_idx, last_nonempty_idx);
+            CompactionResult::OneStepDone
+        } else {
+            CompactionResult::Error
+        }
+    } else {
+        CompactionResult::Error
+    }
+}
+
+fn compute_checksum(disk_blocks: &Vec<Option<u16>>) -> usize {
+    disk_blocks
+        .iter()
+        .enumerate()
+        .fold(0usize, |acc, (idx, block_id)| {
+            acc + idx * block_id.unwrap_or(0) as usize
+        })
+}
+
+fn compact_until_finished(disk_blocks: &mut Vec<Option<u16>>) -> bool {
+    loop {
+        let result = compact_one_block(disk_blocks);
+
+        match result {
+            CompactionResult::Finished => {
+                return true;
+            }
+            CompactionResult::OneStepDone => {
+                continue;
+            }
+            CompactionResult::Error => {
+                return false;
+            }
         }
     }
 }
@@ -23,7 +68,12 @@ fn parse_disk_blocks(input: &str) -> Vec<Option<u16>> {
         .flat_map(|(idx, char)| {
             let idx = idx as u16;
             let id: Option<u16> = if idx % 2 != 0 { None } else { Some(idx / 2) };
-            let qty = format!("{char}").parse::<i16>().unwrap();
+            let qty = match format!("{char}").parse::<i16>() {
+                Ok(x) => x,
+                Err(err) => {
+                    panic!("error parsing char '{char}': {}", err.to_string())
+                }
+            };
             repeat_n(id, qty as usize)
         })
         .collect_vec();
@@ -42,6 +92,31 @@ mod tests {
         "#
         .trim();
         assert_eq!("1928", process(input)?);
+        Ok(())
+
+        // example 1
+        // input: 12345
+        // disk layout of this input is:
+        // 0..111....22222
+        //
+    }
+
+    #[test]
+    fn test_complete_compaction() -> miette::Result<()> {
+        let input = r#"
+2333133121414131402
+        "#
+        .trim();
+
+        let mut disk_blocks = parse_disk_blocks(input);
+        let result = compact_until_finished(&mut disk_blocks);
+        assert!(result);
+
+        assert_eq!(
+            "0099811188827773336446555566..............",
+            render_disk_blocks(&disk_blocks)
+        );
+        assert_eq!(1928usize, compute_checksum(&disk_blocks));
         Ok(())
 
         // example 1
@@ -78,7 +153,7 @@ mod tests {
 2333133121414131402
         "#
         .trim();
-        let mut disk_blocks = parse_disk_blocks(input);
+        let disk_blocks = parse_disk_blocks(input);
 
         let expected = r#"
 00...111...2...333.44.5555.6666.777.888899
@@ -102,7 +177,7 @@ mod tests {
             .into_iter()
             .scan(disk_blocks, |current, idx| {
                 if idx > 0 {
-                    compact_files(current);
+                    compact_one_block(current);
                 }
                 Some(render_disk_blocks(&current))
             })
