cf4d6d2ecbe9320af9d3ba491effeb8580afa2df
Florian Witteler <github@flwi.de>
2024-12-17 12:11:22 +0100

    added all the examples as unit tests

--- adventofcode-2024/day-17/src/part1.rs
+++ adventofcode-2024/day-17/src/part1.rs
@@ -65,6 +65,15 @@ struct Computer {
 
 impl Computer {
     pub(crate) fn run(&mut self) {}
+    pub(crate) fn run_one(&mut self) {}
+
+    // The computer knows eight instructions, each identified by a 3-bit number (called the instruction's opcode).
+    // Each instruction also reads the 3-bit number after it as an input; this is called its operand.
+    //
+    // A number called the instruction pointer identifies the position in the program from which the next opcode will be read;
+    // it starts at 0, pointing at the first 3-bit number in the program.
+    // Except for jump instructions, the instruction pointer increases by 2 after each instruction is processed (to move past the instruction's opcode and its operand).
+    // If the computer tries to read an opcode past the end of the program, it instead halts.
 }
 
 #[cfg(test)]
@@ -82,6 +91,100 @@ Program: 0,1,5,4,3,0
         "#
         .trim();
         assert_eq!("4,6,3,5,6,3,5,2,1,0", process(input)?);
+        Ok(())
+    }
+
+    #[test]
+    fn test_example_1() -> miette::Result<()> {
+        // If register C contains 9, the program 2,6 would set register B to 1.
+        let input = r#"
+Register A: 0
+Register B: 0
+Register C: 9
+
+Program: 2,6
+        "#
+        .trim();
+
+        let mut computer = parse(input)?.1;
+        computer.run();
+        assert_eq!(computer.register_b, 1);
+        Ok(())
+    }
+    #[test]
+    fn test_example_2() -> miette::Result<()> {
+    // If register A contains 10, the program 5,0,5,1,5,4 would output 0,1,2.
+        let input = r#"
+Register A: 10
+Register B: 0
+Register C:
+
+Program: 5,0,5,1,5,4
+        "#
+        .trim();
+
+        let mut computer = parse(input)?.1;
+        computer.run();
+
+        assert_eq!(computer.output, vec![0, 1, 2]);
+        Ok(())
+    }
+
+    #[test]
+    fn test_example_3() -> miette::Result<()> {
+        // If register A contains 2024, the program 0,1,5,4,3,0 would output 4,2,5,6,7,7,7,7,3,1,0 and leave 0 in register A.
+        let input = r#"
+Register A: 2024
+Register B: 0
+Register C:
+
+Program: 0,1,5,4,3,0
+        "#
+        .trim();
+
+        let mut computer = parse(input)?.1;
+        computer.run();
+
+        assert_eq!(computer.output, vec![4, 2, 5, 6, 7, 7, 7, 7, 3, 1, 0]);
+        assert_eq!(computer.register_a, 0);
+        Ok(())
+    }
+
+    #[test]
+    fn test_example_4() -> miette::Result<()> {
+        // If register B contains 29, the program 1,7 would set register B to 26.
+        let input = r#"
+Register A: 0
+Register B: 29
+Register C:
+
+Program: 1,7
+        "#
+        .trim();
+
+        let mut computer = parse(input)?.1;
+        computer.run();
+
+        assert_eq!(computer.register_b, 26);
+        Ok(())
+    }
+
+    #[test]
+    fn test_example_5() -> miette::Result<()> {
+        // If register B contains 2024 and register C contains 43690, the program 4,0 would set register B to 44354.
+        let input = r#"
+Register A: 0
+Register B: 2024
+Register C: 43690
+
+Program: 4,0
+        "#
+        .trim();
+
+        let mut computer = parse(input)?.1;
+        computer.run();
+
+        assert_eq!(computer.register_b, 44354);
         Ok(())
     }
 }
