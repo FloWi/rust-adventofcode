9fa1ef13209e8de0fe5d015bdd146b27456f52c0
Florian Witteler <github@flwi.de>
2024-12-17 19:53:52 +0100

    added Instructions, Operand and OperandType

--- adventofcode-2024/day-17/src/part1.rs
+++ adventofcode-2024/day-17/src/part1.rs
@@ -7,6 +7,7 @@ use nom::combinator::all_consuming;
 use nom::multi::separated_list1;
 use nom::sequence::{preceded, separated_pair, tuple};
 use nom::IResult;
+use num_enum::{FromPrimitive, IntoPrimitive, TryFromPrimitive};
 
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String> {
@@ -23,6 +24,63 @@ pub fn process(input: &str) -> miette::Result<String> {
         .join(",");
 
     Ok(output)
+}
+
+#[derive(TryFromPrimitive, IntoPrimitive, Debug, Clone, Copy)]
+#[repr(u32)] // or u8, i32, etc. depending on your needs
+enum Instruction {
+    ///The adv instruction (opcode 0) performs division. The numerator is the value in the A register.
+    /// The denominator is found by raising 2 to the power of the instruction's combo operand.
+    /// (So, an operand of 2 would divide A by 4 (2^2); an operand of 5 would divide A by 2^B.)
+    /// The result of the division operation is truncated to an integer and then written to the A register.
+    Adv = 0,
+
+    ///The bxl instruction (opcode 1) calculates the bitwise XOR of register B
+    /// and the instruction's literal operand, then stores the result in register B.
+    Bxl = 1,
+
+    ///The bst instruction (opcode 2) calculates the value of its combo operand modulo 8
+    /// (thereby keeping only its lowest 3 bits), then writes that value to the B register.
+    Bst = 2,
+
+    ///The jnz instruction (opcode 3) does nothing if the A register is 0.
+    /// However, if the A register is not zero,
+    /// it jumps by setting the instruction pointer to the value of its literal operand;
+    /// if this instruction jumps, the instruction pointer is not increased by 2 after this instruction.
+    Jnz = 3,
+
+    ///The bxc instruction (opcode 4) calculates the bitwise XOR of register B and register C,
+    /// then stores the result in register B. (For legacy reasons,
+    /// this instruction reads an operand but ignores it.)
+    Bxc = 4,
+
+    ///The out instruction (opcode 5) calculates the value of its combo operand modulo 8,
+    /// then outputs that value.
+    /// (If a program outputs multiple values, they are separated by commas.)
+    Out = 5,
+
+    ///The bdv instruction (opcode 6) works exactly like the adv instruction except that the result is stored in the B register.
+    /// (The numerator is still read from the A register.)
+    Bdv = 6,
+
+    ///The cdv instruction (opcode 7) works exactly like the adv instruction except that the result is stored in the C register.
+    /// (The numerator is still read from the A register.)
+    Cdv = 7,
+}
+
+impl Instruction {
+    pub fn operand_type(&self) -> OperandType {
+        match self {
+            Instruction::Adv => OperandType::Combo,
+            Instruction::Bxl => OperandType::Literal,
+            Instruction::Bst => OperandType::Combo,
+            Instruction::Jnz => OperandType::Literal,
+            Instruction::Bxc => OperandType::Ignored,
+            Instruction::Out => OperandType::Combo,
+            Instruction::Bdv => OperandType::Combo,
+            Instruction::Cdv => OperandType::Combo,
+        }
+    }
 }
 
 fn parse(input: &str) -> IResult<&str, Computer> {
@@ -46,7 +104,10 @@ fn parse(input: &str) -> IResult<&str, Computer> {
             register_a: registers[0],
             register_b: registers[1],
             register_c: registers[2],
-            program,
+            program: program
+                .into_iter()
+                .map(|instruction_u32| Instruction::try_from(instruction_u32).unwrap())
+                .collect_vec(),
             instruction_pointer: 0,
             output: vec![],
         },
@@ -58,14 +119,36 @@ struct Computer {
     register_a: u32,
     register_b: u32,
     register_c: u32,
-    program: Vec<u32>,
-    instruction_pointer: u32,
+    program: Vec<Instruction>,
+    instruction_pointer: usize,
     output: Vec<u32>,
 }
 
+#[derive(Debug)]
+enum OperandType {
+    Combo,
+    Literal,
+    Ignored,
+}
+
 impl Computer {
-    pub(crate) fn run(&mut self) {}
-    pub(crate) fn run_one(&mut self) {}
+    pub(crate) fn run(&mut self) {
+        self.run_one();
+    }
+    pub(crate) fn run_one(&mut self) {
+        let instruction = self.current_instruction();
+        let operand = self.current_operand();
+        let operand_type = operand.operand_type();
+
+        dbg!(instruction, operand, operand_type);
+    }
+
+    pub(crate) fn current_instruction(&self) -> Instruction {
+        self.program[self.instruction_pointer]
+    }
+    pub(crate) fn current_operand(&self) -> Instruction {
+        self.program[self.instruction_pointer + 1]
+    }
 
     // The computer knows eight instructions, each identified by a 3-bit number (called the instruction's opcode).
     // Each instruction also reads the 3-bit number after it as an input; this is called its operand.
@@ -106,14 +189,14 @@ Program: 2,6
         "#
         .trim();
 
-        let mut computer = parse(input)?.1;
+        let mut computer = parse(input).unwrap().1;
         computer.run();
         assert_eq!(computer.register_b, 1);
         Ok(())
     }
     #[test]
     fn test_example_2() -> miette::Result<()> {
-    // If register A contains 10, the program 5,0,5,1,5,4 would output 0,1,2.
+        // If register A contains 10, the program 5,0,5,1,5,4 would output 0,1,2.
         let input = r#"
 Register A: 10
 Register B: 0
@@ -123,7 +206,7 @@ Program: 5,0,5,1,5,4
         "#
         .trim();
 
-        let mut computer = parse(input)?.1;
+        let mut computer = parse(input).unwrap().1;
         computer.run();
 
         assert_eq!(computer.output, vec![0, 1, 2]);
@@ -142,7 +225,7 @@ Program: 0,1,5,4,3,0
         "#
         .trim();
 
-        let mut computer = parse(input)?.1;
+        let mut computer = parse(input).unwrap().1;
         computer.run();
 
         assert_eq!(computer.output, vec![4, 2, 5, 6, 7, 7, 7, 7, 3, 1, 0]);
@@ -162,7 +245,7 @@ Program: 1,7
         "#
         .trim();
 
-        let mut computer = parse(input)?.1;
+        let mut computer = parse(input).unwrap().1;
         computer.run();
 
         assert_eq!(computer.register_b, 26);
@@ -181,7 +264,7 @@ Program: 4,0
         "#
         .trim();
 
-        let mut computer = parse(input)?.1;
+        let mut computer = parse(input).unwrap().1;
         computer.run();
 
         assert_eq!(computer.register_b, 44354);
