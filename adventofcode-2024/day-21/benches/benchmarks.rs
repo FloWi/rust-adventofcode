8f1615b138c13c5ce70eabfe0652c89eb4712275
Florian Witteler <github@flwi.de>
2024-12-26 13:47:44 +0100

    set benchmark sample count back to 100 (because it's faster now)

--- adventofcode-2024/day-21/benches/benchmarks.rs
+++ adventofcode-2024/day-21/benches/benchmarks.rs
@@ -5,12 +5,12 @@ fn main() {
     divan::main();
 }
 
-#[divan::bench(sample_count = 5)]
+#[divan::bench]
 fn part1() {
     part1::process(divan::black_box(include_str!("../input1.txt",))).unwrap();
 }
 
-#[divan::bench(sample_count = 5)]
+#[divan::bench]
 fn part2() {
     part2::process(divan::black_box(include_str!("../input2.txt",))).unwrap();
 }
