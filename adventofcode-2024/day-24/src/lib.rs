d6cb70d091bc9ed98d7019054c96f3ca1b6c9b44
Florian Witteler <github@flwi.de>
2025-01-03 11:23:12 +0100

    day-24 part 1 now solved by new AocComputer

--- adventofcode-2024/day-24/src/lib.rs
+++ adventofcode-2024/day-24/src/lib.rs
@@ -2,4 +2,3 @@ extern crate core;
 
 pub mod part1;
 pub mod part2;
-mod part2_rewrite;
--- adventofcode-2024/day-24/src/part1.rs
+++ adventofcode-2024/day-24/src/part1.rs
@@ -9,73 +9,233 @@ use nom::combinator::value;
 use nom::multi::separated_list1;
 use nom::sequence::{preceded, separated_pair, tuple};
 use nom::{IResult, Parser};
-use std::collections::{HashMap, VecDeque};
+use petgraph::dot::Dot;
+use petgraph::prelude::DiGraphMap;
+use rand::Rng;
+use std::collections::{HashMap, HashSet, VecDeque};
 use std::fmt::{Display, Formatter};
-use std::ops::{BitAnd, BitOr, BitXor, Not};
-use tracing::info;
+use std::ops::{BitAnd, BitOr, BitXor, Not, RangeInclusive};
+use tracing::{debug, info};
 
-#[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String> {
-    let (_, (initial_signals, gates)): (&str, (HashMap<Signal, bool>, Vec<Gate>)) =
-        parse(input).map_err(|e| miette!("parse failed {}", e))?;
+    let (_, mut aoc_computer) = parse(input).map_err(|e| miette!("parse failed {}", e))?;
 
-    let final_signals = evaluate_dag(&initial_signals, gates);
-    let binary: String = final_signals
-        .into_iter()
-        .filter(|(key, _)| key.0.starts_with("z"))
-        .sorted_by_key(|(key, _)| key.0)
-        .rev() // most significant bit first
-        .map(|(_, value)| (value as u8).to_string())
-        .collect();
+    aoc_computer.run_computer();
 
-    let result = u64::from_str_radix(&binary, 2).unwrap();
-    Ok(result.to_string())
+    Ok(aoc_computer.z.to_string())
 }
 
-#[derive(Eq, PartialEq, Hash, Debug, Clone)]
-struct Signal<'a>(&'a str);
+struct AocComputer {
+    gates: Vec<Gate>,
+    initial_signals: HashMap<String, bool>,
+    x: u64,
+    y: u64,
+    z: u64,
+    intermediate_signals: Vec<Option<u8>>,
+    intermediate_signal_names: Vec<String>,
+    intermediate_mapping: HashMap<String, usize>,
+    indexed_gates: Vec<IndexedGate>,
+}
 
-#[derive(Debug, Clone)]
+impl AocComputer {
+    fn new(gates: Vec<Gate>, input_signals: HashMap<String, bool>) -> Self {
+        let intermediate_signal_names = gates
+            .iter()
+            .flat_map(|g| [g.in_1.clone(), g.in_2.clone(), g.out.clone()])
+            .filter(|signal_name| {
+                !signal_name.starts_with('x')
+                    && !signal_name.starts_with('y')
+                    && !signal_name.starts_with('z')
+            })
+            .unique()
+            .sorted()
+            .collect_vec();
+
+        let mut intermediate_signals = Vec::with_capacity(intermediate_signal_names.len());
+        for _ in 0..intermediate_signal_names.len() {
+            intermediate_signals.push(None);
+        }
+        let intermediate_index_mapping: HashMap<String, usize> = intermediate_signal_names
+            .iter()
+            .enumerate()
+            .map(|(idx, name)| (name.clone(), idx))
+            .collect();
+
+        let indexed_gates = gates
+            .iter()
+            .map(|g| {
+                let in_1 = Self::get_register(&g.in_1, &intermediate_index_mapping);
+                let in_2 = Self::get_register(&g.in_2, &intermediate_index_mapping);
+                let out = Self::get_register(&g.out, &intermediate_index_mapping);
+                IndexedGate {
+                    in_1,
+                    in_2,
+                    op: g.op,
+                    out,
+                }
+            })
+            .collect_vec();
+
+        let mut computer = Self {
+            gates,
+            initial_signals: input_signals.clone(),
+            x: 0,
+            y: 0,
+            z: 0,
+            intermediate_signals,
+            intermediate_signal_names,
+            intermediate_mapping: intermediate_index_mapping.clone(),
+            indexed_gates,
+        };
+
+        //initialize x and y using the mechanics we already have in place
+        for (signal_name, value) in input_signals {
+            let memory_location =
+                AocComputer::get_register(&signal_name, &intermediate_index_mapping);
+            computer.set(&memory_location, if value { 1 } else { 0 });
+        }
+        computer
+    }
+
+    fn reset(&mut self) {
+        self.x = 0;
+        self.y = 0;
+        self.z = 0;
+
+        for elem in self.intermediate_signals.iter_mut() {
+            *elem = None;
+        }
+    }
+
+    fn get_register(
+        signal_name: &str,
+        intermediate_index_mapping: &HashMap<String, usize>,
+    ) -> MemoryLocation {
+        match signal_name.split_at(1) {
+            ("x", idx) => MemoryLocation::X(idx.parse::<usize>().unwrap()),
+            ("y", idx) => MemoryLocation::Y(idx.parse::<usize>().unwrap()),
+            ("z", idx) => MemoryLocation::Z(idx.parse::<usize>().unwrap()),
+            _ => {
+                let idx = intermediate_index_mapping
+                    .get(&signal_name.to_string())
+                    .unwrap();
+                MemoryLocation::Intermediate(*idx)
+            }
+        }
+    }
+
+    fn get(&self, memory_location: &MemoryLocation) -> Option<u8> {
+        match memory_location {
+            MemoryLocation::X(idx) => Some(u8::try_from((self.x >> idx) & 1).unwrap()),
+            MemoryLocation::Y(idx) => Some(u8::try_from((self.y >> idx) & 1).unwrap()),
+            MemoryLocation::Z(idx) => Some(u8::try_from((self.z >> idx) & 1).unwrap()),
+            MemoryLocation::Intermediate(idx) => match self.intermediate_signals.get(*idx) {
+                Some(Some(num)) => Some(*num),
+                _ => None,
+            },
+        }
+    }
+    fn set(&mut self, memory_location: &MemoryLocation, value: u8) {
+        match memory_location {
+            MemoryLocation::X(idx) => {
+                let mask = 1 << *idx;
+                self.x = (self.x & !mask) | (u64::from(value) << idx);
+            }
+            MemoryLocation::Y(idx) => {
+                let mask = 1 << *idx;
+                self.y = (self.y & !mask) | (u64::from(value) << idx);
+            }
+            MemoryLocation::Z(idx) => {
+                let mask = 1 << *idx;
+                self.z = (self.z & !mask) | (u64::from(value) << idx);
+            }
+            MemoryLocation::Intermediate(idx) => {
+                self.intermediate_signals[*idx] = Some(value);
+            }
+        }
+    }
+
+    fn execute(&mut self, gate: &IndexedGate) -> ExecutionResult {
+        let in_1 = self.get(&gate.in_1);
+        let in_2 = self.get(&gate.in_2);
+        match in_1.zip(in_2) {
+            None => ExecutionResult::NotAllInputsAvailable,
+            Some((in_1, in_2)) => {
+                let result: u8 = gate.op.eval(in_1, in_2);
+                self.set(&gate.out, result);
+                ExecutionResult::Ok
+            }
+        }
+    }
+
+    fn run_computer(&mut self) {
+        let mut operations = VecDeque::from_iter(self.indexed_gates.clone());
+
+        // alternative if cloning is too slow
+        // putting all the indices of the indexed_gates into the queue and accessing them like this
+        // let foo = &self.indexed_gates[0];
+        while let Some(indexed_gate) = operations.pop_front() {
+            match self.execute(&indexed_gate) {
+                ExecutionResult::NotAllInputsAvailable => {
+                    operations.push_back(indexed_gate);
+                }
+                ExecutionResult::Ok => {}
+            }
+        }
+    }
+}
+
+enum ExecutionResult {
+    Ok,
+    NotAllInputsAvailable,
+}
+
+#[derive(Debug, Clone, Copy, Eq, Hash, PartialEq)]
 enum Operator {
     XOR,
     OR,
     AND,
 }
 
-impl Display for Operator {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        let label = match self {
-            Operator::XOR => "XOR",
-            Operator::OR => "OR",
-            Operator::AND => "AND",
-        };
-        f.write_str(label)
-    }
-}
-
-#[derive(Debug)]
-struct Gate<'a> {
-    in_1: Signal<'a>,
-    in_2: Signal<'a>,
-    op: Operator,
-    out: Signal<'a>,
-}
-
-impl Gate<'_> {
-    pub(crate) fn eval(&self, in1: bool, in2: bool) -> bool {
-        match self.op {
-            Operator::XOR => in1.bitxor(in2),
-            Operator::OR => in1.bitor(in2),
-            Operator::AND => in1.bitand(in2),
+impl Operator {
+    pub(crate) fn eval(&self, in_1: u8, in_2: u8) -> u8 {
+        match self {
+            Operator::XOR => BitXor::bitxor(in_1, in_2),
+            Operator::OR => BitOr::bitor(in_1, in_2),
+            Operator::AND => BitAnd::bitand(in_1, in_2),
         }
     }
 }
 
-fn parse(input: &str) -> IResult<&str, (HashMap<Signal, bool>, Vec<Gate>)> {
+#[derive(Debug, Clone, Eq, Hash, PartialEq)]
+struct Gate {
+    in_1: String,
+    in_2: String,
+    op: Operator,
+    out: String,
+}
+
+#[derive(Debug, Clone, Eq, Hash, PartialEq)]
+enum MemoryLocation {
+    X(usize),
+    Y(usize),
+    Z(usize),
+    Intermediate(usize),
+}
+
+#[derive(Debug, Clone, Eq, Hash, PartialEq)]
+struct IndexedGate {
+    in_1: MemoryLocation,
+    in_2: MemoryLocation,
+    op: Operator,
+    out: MemoryLocation,
+}
+
+fn parse(input: &str) -> IResult<&str, AocComputer> {
     let (input, initial_map) = separated_list1(
         line_ending,
         separated_pair(
-            alphanumeric1.map(|s: &str| Signal(s)),
+            alphanumeric1,
             tag(": "),
             complete::u8.map(|num| match num {
                 1 => true,
@@ -94,59 +254,38 @@ fn parse(input: &str) -> IResult<&str, (HashMap<Signal, bool>, Vec<Gate>)> {
             line_ending,
             separated_pair(
                 tuple((
-                    alphanumeric1.map(|s: &str| Signal(s)),
+                    alphanumeric1,
                     alt((
                         value(Operator::AND, tag(" AND ")),
                         value(Operator::OR, tag(" OR ")),
                         value(Operator::XOR, tag(" XOR ")),
                     )),
-                    alphanumeric1.map(|s: &str| Signal(s)),
+                    alphanumeric1,
                 )),
                 tag(" -> "),
-                alphanumeric1.map(|s: &str| Signal(s)),
-            )
-            .map(|((in_1, op, in_2), out)| Gate {
-                in_1,
-                in_2,
-                op,
-                out,
-            }),
+                alphanumeric1,
+            ),
         ),
     )(input)?;
 
-    Ok((input, (initial_map.into_iter().collect(), gates)))
-}
-
-fn evaluate_dag<'a>(
-    initial_signals: &'a HashMap<Signal<'a>, bool>,
-    gates: Vec<Gate<'a>>,
-) -> HashMap<Signal<'a>, bool> {
-    let mut signals = initial_signals.clone();
-
-    let mut open_list_gates: VecDeque<Gate> = gates.into();
-
-    while open_list_gates.is_empty().not() {
-        if let Some(pos) = open_list_gates
-            .iter()
-            .position(|g| signals.contains_key(&g.in_1) && signals.contains_key(&g.in_2))
-        {
-            let gate = open_list_gates.remove(pos).unwrap();
-            let signal_in_1 = &gate.in_1;
-            let signal_in_2 = &gate.in_2;
-            let op = &gate.op;
-            let in_1 = signals[signal_in_1];
-            let in_2 = signals[signal_in_2];
-
-            let out = gate.eval(in_1, in_2);
-            signals.insert(gate.out.clone(), out);
-            info!(
-                "evaluated {} {} {} = {:?} ==> {} {} {} = {}",
-                signal_in_1.0, op, signal_in_2.0, gate.out.0, in_1, op, in_2, out
-            )
-        }
-    }
-
-    signals
+    Ok((
+        input,
+        AocComputer::new(
+            gates
+                .into_iter()
+                .map(|((in_1, op, in_2), out)| Gate {
+                    in_1: in_1.to_string(),
+                    in_2: in_2.to_string(),
+                    op,
+                    out: out.to_string(),
+                })
+                .collect_vec(),
+            initial_map
+                .into_iter()
+                .map(|(k, v)| (k.to_string(), v))
+                .collect(),
+        ),
+    ))
 }
 
 #[cfg(test)]
@@ -155,23 +294,6 @@ mod tests {
 
     #[test]
     fn test_process_small_example() -> miette::Result<()> {
-        let input = r#"
-x00: 1
-x01: 1
-x02: 1
-y00: 0
-y01: 1
-y02: 0
-
-x00 AND y00 -> z00
-x01 XOR y01 -> z01
-x02 OR y02 -> z02      "#
-            .trim();
-        assert_eq!("4", process(input)?);
-        Ok(())
-    }
-    #[test]
-    fn test_process_large_example() -> miette::Result<()> {
         let input = r#"
 x00: 1
 x01: 0
@@ -222,7 +344,35 @@ tgd XOR rvg -> z12
 tnw OR pbm -> gnj
       "#
         .trim();
-        assert_eq!("2024", process(input)?);
+
+        let (_, mut actual_computer) = parse(input).unwrap();
+        assert_eq!(actual_computer.gates.len(), 36);
+        assert_eq!(actual_computer.indexed_gates.len(), 36);
+        assert_eq!(actual_computer.x, 13);
+        assert_eq!(actual_computer.y, 31);
+        actual_computer.run_computer();
+
+        // currently the result is wrong, but this is the expected output
+        assert_eq!(actual_computer.z, 2024);
+
+        let expected_z = actual_computer.x + actual_computer.y;
+        let diff_bits = (actual_computer.z ^ expected_z);
+        let num_diff_bits = diff_bits.count_ones();
+        assert_eq!(num_diff_bits, 6);
+        println!("  actual_z: {:b}", actual_computer.z);
+        println!("expected_z: {:b}", expected_z);
+        println!(" diff_bits: {:b}", diff_bits);
+
+        //  actual_z: 11111101000
+        //expected_z:      101100
+        // diff_bits: 11111000100
+
+        actual_computer.reset();
+
+        assert_eq!(actual_computer.x, 0);
+        assert_eq!(actual_computer.y, 0);
+        assert_eq!(actual_computer.z, 0);
+
         Ok(())
     }
 }
